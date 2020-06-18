42f377e30f82985b84fd22b9d3a1811827295b07
MarcelleK <paulemarcellekassi@gmail.com>
2020-06-18 15:47:14 +0200

    [INTERFACE]Add menu options

--- app/src/main/java/org/libreapps/yourtrad/MainActivity.java
+++ app/src/main/java/org/libreapps/yourtrad/MainActivity.java
@@ -3,6 +3,8 @@ package org.libreapps.yourtrad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
